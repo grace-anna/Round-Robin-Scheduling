64082bc81449a7090c998b41047f3772fb927f54
Grace Anna <graceannavarghese05@gmail.com>
2026-02-26 18:09:32 +0530

    Update vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Round-Robin-Scheduling/', // Match the GitHub repo name EXACTLY
 })
